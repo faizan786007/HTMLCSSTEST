4dece9618a8c8189772c74d0d66235f5b239724a
faizan786007 <kingsmanfaizanali@gmail.com>
2024-09-28 15:33:07 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// const leftBtn = document.getElementById("leftBtn");
+// const rightBtn = document.getElementById("rightBtn");
+// const scrollContainer = document.querySelector(".scroll-container");
+
+// rightBtn.addEventListener("click", () => {
+// 	scrollContainer.scrollBy({ left: 300, behavior: "smooth" });
+// });
+
+// leftBtn.addEventListener("click", () => {
+// 	scrollContainer.scrollBy({ left: -300, behavior: "smooth" });
+// });
