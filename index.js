6b19864a847fd2fabd859ecc6e0b473a19780755
faizan786007 <kingsmanfaizanali@gmail.com>
2024-09-30 11:31:20 +0530

    updates

--- index.js
+++ index.js
@@ -9,3 +9,70 @@
 // leftBtn.addEventListener("click", () => {
 // 	scrollContainer.scrollBy({ left: -300, behavior: "smooth" });
 // });
+
+/************************************theory ques ******************************/
+// 1. What is HTML?
+/*
+ html is hypertext markup lang used to create n design web pages
+*/
+// 2. Explain the structure of an HTML document.
+/*
+   structure is well defined contain DOCTYPE html in html 5 format
+   lang='eng'
+   head contain meta data about page like document title,viewport ,encoding
+   body contain all content parrt
+   that user see like button ,img,link,text
+*/
+// 3. What are elements, attributes, and values in HTML?
+/*
+element ==>eg---<p>this is paragraph</p>
+attribute==> <tagname attribute="value "> content u write</tagname>
+value==>which we give to attribute eg--in img tag src is attribute and img.png is value of it
+alt is atrribute n value ois given to it
+*/
+// 4. What is the difference between block-level and inline elements?
+/*
+block -start in new line,take full width of page,take height based on content
+inline-remain in same line ,take only required width,expand as wide as content,height depent on content height
+p is block n span ,a,img are inline
+
+*/
+// How do you create a basic HTML table?
+{
+	/* <table>
+  <thead>
+    <tr>
+      <th>H1</th>
+      <th>H2</th>
+      <th>H3</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Row 1, Col 1</td>
+      <td>Row 1, Col2</td>
+      <td>Row 1, Col 3</td>
+    </tr>
+    <tr>
+      <td>Row 2, Col1</td>
+      <td>Row 2, Col2</td>
+      <td>Row 2, Col3</td>
+    </tr>
+  </tbody>
+</table> */
+}
+
+// What is semantic HTML and why is it important?
+/*
+semantic html give more meaning to page ,it help coder n browser to get more readibily,accessbility n seo helpful
+ex-header,footer,aside,article,main,section
+
+*/
+// Explain the purpose of <header>, <nav>, <main>, <footer>, and <aside>
+// elements.
+/*
+header-title n logo
+nav-nav links,main site menu, pagination, or other navigational blocks.
+main-conatin the  primary content of the document
+footer-conatiner contact info,links social media,legal disclamer
+*/
